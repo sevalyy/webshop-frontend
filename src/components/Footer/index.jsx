791ab25270c5049666894e4eb53a7d024abd87c3
sevalyy <seval@yamanyar.com>
2022-06-09 15:34:04 +0200

    footer without category pages

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,46 @@
+import { Link } from "react-router-dom";
 import "./style.css";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="footer">FOOTER</div>
+    <div className="footer">
+      <div className="logo">
+        {" "}
+        <h3>
+          The<span className="footerLogo">Shop</span>
+        </h3>
+      </div>
+      <div className="categories">
+        <h4>CATEGORIES</h4>
+        <div className="categoryLink visit">
+          <p>
+            <Link to={"/shop"}>Electronics</Link>
+          </p>
+          <p>
+            <Link to={"/shop"}>Jewelery</Link>
+          </p>
+          <p>
+            <Link to={"/shop"}>Men's Clothing</Link>
+          </p>
+          <p>
+            <Link to={"/shop"}>Women's Clothing</Link>
+          </p>
+        </div>
+      </div>
+      <div className="account">
+        <h4>ACCOUNT</h4>
+        <p>My Account</p>
+        <p>Returns</p>
+        <p>Order History</p>
+        <p>Order Tracking</p>
+      </div>
+      <div className="follow">
+        <h4>FOLLOW</h4>
+        <p>
+          <FaFacebook /> <FaInstagram /> <FaTwitter />
+        </p>
+      </div>
     </div>
   );
 };
